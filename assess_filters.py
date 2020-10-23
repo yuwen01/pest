098fc77f4020d624f2ea1e84b625a06240504b55
Yuwen Zhang <yuwen01@berkeley.edu>
2020-10-22 23:14:34 -0700

    some early shit

--- assess_filters.py
+++ assess_filters.py
@@ -2,11 +2,18 @@ import cv2
 import numpy as np
 import utils
 import xml.etree.ElementTree as ET
+import bug_detection
 
-
-def read_content(num):
-    im_path = "stickytraps/" + str(num) + ".jpg"
-    xml_path = "stickytraps/" + str(num) + ".xml"
+'''
+parameters:
+img_num: the number of the image to look at
+detector: the detector function, which takes in an image path, and outputs the number of bugs on that image
+display_boxes: whether or not to display the legit bounding boxes
+display_bugs: whether or not to display the detected bugs.
+'''
+def assess_detector(img_num, detector, display_boxes=False, display_bugs=False):
+    im_path = "stickytraps/" + str(img_num) + ".jpg"
+    xml_path = "stickytraps/" + str(img_num) + ".xml"
 
     img = cv2.imread(im_path)
 
@@ -14,39 +21,50 @@ def read_content(num):
     root = tree.getroot()
 
     list_with_all_boxes = []
+    bugs = {'WF': 0, 'MR': 0, 'NC': 0}
 
     for boxes in root.iter('object'):
-
-        #filename = root.find('filename').text
 
         ymin, xmin, ymax, xmax = None, None, None, None
 
         for box in boxes.findall("bndbox"):
-            ymin = int(box.find("ymin").text)
+            ymin = img.shape[1] - int(box.find("ymin").text)
             xmin = int(box.find("xmin").text)
-            ymax = int(box.find("ymax").text)
+            ymax = img.shape[1] - int(box.find("ymax").text)
             xmax = int(box.find("xmax").text)
-            # if str(boxes.find('name').text) == "WF":
-            #     cv2.rectangle(img, (ymin, xmin), (ymax, xmax), (255, 0, 255), 8)
-            # if str(boxes.find('name').text) == "MR":
-            #     cv2.rectangle(img, (ymin, xmin), (ymax, xmax), (0, 0, 0), 8)
-            # if str(boxes.find('name').text) == "NC":
-            #     cv2.rectangle(img, (ymin, xmin), (ymax, xmax), (0, 0, 255), 8)
+            name = str(boxes.find('name').text)
+            if name == "WF":
+                cv2.rectangle(img, (ymin, xmin), (ymax, xmax), (255, 0, 255), 8)
+            if name == "MR":
+                cv2.rectangle(img, (ymin, xmin), (ymax, xmax), (0, 0, 0), 8)
+            if name == "NC":
+                cv2.rectangle(img, (ymin, xmin), (ymax, xmax), (0, 0, 255), 8)
+            bugs[name] += 1
 
-        
-    cv2.rectangle(img, (242, 1083), (416, 1184), (0, 0, 255), 8)
+    if display_boxes:
+        cv2.rectangle(img, (242, 1083), (416, 1184), (0, 0, 255), 8)
+        scale_percent = 20  # percent of original size
+        width = int(img.shape[1] * scale_percent / 100)
+        height = int(img.shape[0] * scale_percent / 100)
+        dim = (width, height)
+        img  = cv2.resize(img, dim)
+        cv2.imshow("Labelled", img)
+    
+    num_detected = detector(im_path, display=display_bugs)
 
-    scale_percent = 20  # percent of original size
-    width = int(img.shape[1] * scale_percent / 100)
-    height = int(img.shape[0] * scale_percent / 100)
-    dim = (width, height)
-    img  = cv2.resize(img, dim)
-    cv2.imshow("Labelled", img)
+    print('Img number:\t', img_num)
+    print("Legit WF:\t", bugs['WF'], '\nLegit MR:\t', bugs['MR'], '\nLegit NC:\t', bugs['NC'])
+    print("Legit total:\t", bugs['WF'] + bugs['MR'] + bugs['NC'])
+    print("Legit sans WF:\t", bugs['MR'] + bugs['NC'])
+    print('Detected total:\t', num_detected)
+    print()
+    print()
 
     #return filename, list_with_all_boxes
 
 
 if __name__ == '__main__':
-    read_content(1013)
+    for i in range(1000, 1030):
+        assess_detector(i, bug_detection.detector)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
--- bug_detection.py
+++ bug_detection.py
@@ -2,59 +2,61 @@ import cv2
 import numpy as np
 import utils
 
-# start with colored image
-pathImage = "stickytraps/1170.jpg"
-widthImg = 600
-heightImg = 800
+# Returns the number of 
+def detector(im_path, display=False):
+    # start with colored image
+    widthImg = 600
+    heightImg = 800
 
-#Gray scale and blur the image
-img = cv2.imread(pathImage)
-imgGray = cv2.imread(pathImage, cv2.IMREAD_GRAYSCALE)
-imgBlur = cv2.GaussianBlur(imgGray, (7, 7), 1)
-height, width, channels = img.shape
+    #Gray scale and blur the image
+    img = cv2.imread(im_path)
+    imgGray = cv2.imread(im_path, cv2.IMREAD_GRAYSCALE)
+    imgBlur = cv2.GaussianBlur(imgGray, (7, 7), 1)
+    height, width, channels = img.shape
 
-# Setup SimpleBlobDetector parameters.
-params = cv2.SimpleBlobDetector_Params()
+    # Setup SimpleBlobDetector parameters.
+    params = cv2.SimpleBlobDetector_Params()
 
-# Filter by Area.
-params.filterByArea = True
-params.minArea = 400
-#params.maxArea = 3000
+    # Filter by Area.
+    params.filterByArea = True
+    params.minArea = 400
+    #params.maxArea = 3000
 
-# Filter by Convexity
-params.filterByConvexity = True
-params.minConvexity = 0.00001
+    # Filter by Convexity
+    params.filterByConvexity = True
+    params.minConvexity = 0.00001
 
-# Filter by Inertia
-params.filterByInertia = True
-params.minInertiaRatio = 0.000001
-
-
-# Create a detector with the parameters
-detector = cv2.SimpleBlobDetector_create(params)
-
-#detector = cv2.SimpleBlobDetector_create()
-keypoints = detector.detect(imgBlur)
-print(len(keypoints))
-imgKeyPoints = cv2.drawKeypoints(img, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    # Filter by Inertia
+    params.filterByInertia = True
+    params.minInertiaRatio = 0.000001
 
 
-# Crop out keypoints
-str = ""
-for keypoint in keypoints:
-    str += "1"
-    x = int(keypoint.pt[0])
-    y = int(keypoint.pt[1])
-    size = int(keypoint.size)
-    cv2.imshow(str, img[max(1,y-2*size): min(height-1,y+2*size), max(1,x-2*size): min(width-1,x+2*size)])
+    # Create a detector with the parameters
+    detector = cv2.SimpleBlobDetector_create(params)
+
+    #detector = cv2.SimpleBlobDetector_create()
+    keypoints = detector.detect(imgBlur)
+    #print(len(keypoints))
+    imgKeyPoints = cv2.drawKeypoints(img, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+    if display:
+        # Crop out keypoints
+        str = ""
+        for keypoint in keypoints:
+            str += "1"
+            x = int(keypoint.pt[0])
+            y = int(keypoint.pt[1])
+            size = int(keypoint.size)
+            cv2.imshow(str, img[max(1,y-2*size): min(height-1,y+2*size), max(1,x-2*size): min(width-1,x+2*size)])
 
 
-# Display found keypoints
-imgKeyPoints = cv2.resize(imgKeyPoints, (2000, 1400))
-cv2.imshow("Keypoints", imgKeyPoints)
-#cv2.imshow("grays", imgGray)
-#cv2.imshow("blur", imgBlur)
-#cv2.imshow("image", img)
+        # Display found keypoints
+        imgKeyPoints = cv2.resize(imgKeyPoints, (2000, 1400))
+        cv2.imshow("Keypoints", imgKeyPoints)
+        #cv2.imshow("grays", imgGray)
+        #cv2.imshow("blur", imgBlur)
+        #cv2.imshow("image", img)
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    return len(keypoints)
